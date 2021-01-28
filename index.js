ce9e74a0841f93894d8e51ed859257a07533d3aa
BuildTools <unconfigured@null.spigotmc.org>
2021-01-28 22:15:52 +0800

    change back to 5000 port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 // To run go to terminal and type in : npm run dev
 app.get('/', (req, res) => {res.send('<h1>Hello World!</h1>')});
 
-const PORT = process.env.port || 443;
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, ()=> console.log(`Server Started on port ${PORT}`));
 

748eb6a40059da32e94e219a5498b5a1ca60a815
BuildTools <unconfigured@null.spigotmc.org>
2021-01-28 21:35:53 +0800

    change port num

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// To run go to terminal and type in : npm run dev
 app.get('/', (req, res) => {res.send('<h1>Hello World!</h1>')});
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 443;
 
 app.listen(PORT, ()=> console.log(`Server Started on port ${PORT}`));
+

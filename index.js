cbfd14862df51cc1a3e0174a42ceeb5d7052e5d7
Ryanntc18 <56615915+Ryanntc18@users.noreply.github.com>
2021-01-29 11:47:32 +0800

    Inserted html files to test out

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require('express');
 
 const app = express();
 
-// To run go to terminal and type in : npm run dev
-app.get('/', (req, res) => {res.send('<h1>Hello World!</h1>')});
-
 const PORT = process.env.port || 5000;
 
 app.listen(PORT, ()=> console.log(`Server Started on port ${PORT}`));
 
+// To run go to terminal and type in : npm run dev
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile('./views/dashboard.html', {root: __dirname});
+});
+
+app.get('/dashboard-free', (req, res) => {
+    res.redirect('./views/dashboard.html');
+});
+
+//Error 404 
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404error.html', {root: __dirname});
+});

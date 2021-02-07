efd556f9fa676ca82bd28dc377808a73f72dc8b4
BuildTools <unconfigured@null.spigotmc.org>
2021-02-07 10:39:36 +0800

    directory fix 1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // middleware (for logging)
-app.use(express.static('public'));
+app.use(express.static(__dirname + '../public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
